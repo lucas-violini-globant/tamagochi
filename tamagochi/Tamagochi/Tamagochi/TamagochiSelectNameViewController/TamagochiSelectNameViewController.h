f0846edd58f18481b9eab38a19fe3d721d8b67aa
Lucas Violini <contacto.lucas.globant@gmail.com>
2014-11-18 16:54:09 -0300

    Primer Commit

---
+++ tamagochi/Tamagochi/Tamagochi/TamagochiSelectNameViewController/TamagochiSelectNameViewController.h
@@ -0,0 +1,13 @@
+//
+//  TamagochiSelectNameViewController.h
+//  Tamagochi
+//
+//  Created by Lucas on 11/18/14.
+//  Copyright (c) 2014 Lucas. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TamagochiSelectNameViewController : UIViewController
+
+@end
---
+++ tamagochi/Tamagochi/Tamagochi/TamagochiStatusViewController/TamagochiStatusViewController.h
@@ -0,0 +1,13 @@
+//
+//  TamagochiStatusViewController.h
+//  Tamagochi
+//
+//  Created by Lucas on 11/18/14.
+//  Copyright (c) 2014 Lucas. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TamagochiStatusViewController : UIViewController
+
+@end
